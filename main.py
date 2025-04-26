570d03df3b7fb7a47766459850c0f3b7d2a7729a
Andre <coronel.filho@hotmail.com>
2025-04-26 10:47:43 -0300

    primeiro prototipo

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# criando a aplicação
+app = FastAPI() #instância de aplicação
+
+# Servir arquivos estáticos (CSS, imagens, etc)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Definindo onde estão os templates (HTML)
+templates = Jinja2Templates(directory="templates")
+
+# Rota inicial
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# Rota para receber mensagem do usuário
+@app.post("/chat", response_class=HTMLResponse)
+async def chat(request: Request, message: str = Form(...)):
+    # Lógica simples de resposta automática
+    resposta = processar_mensagem(message)
+    
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "resposta": resposta,
+        "mensagem_usuario": message
+    })
+
+# Função p gerar resposta automática
+def processar_mensagem(mensagem):
+    mensagem = mensagem.lower()
+    if "campeonatos" in mensagem:
+            return "A FURIA está participando da IEM Katowice e de outros torneios incríveis!"
+    elif "time" in mensagem:
+        return "O lineup atual da FURIA é formado por KSCERATO, yuurih, chelo, FalleN e arT."
+    elif "loja" in mensagem:
+        return "Confira os produtos oficiais na loja: https://loja.furia.gg/"
+    else:
+        return "Desculpe, não entendi sua pergunta. Tente perguntar sobre campeonatos, time ou loja!"
